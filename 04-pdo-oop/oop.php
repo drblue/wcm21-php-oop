d7b59f7af91e2613fcd53e5f655b6af78f0f9ab1
Johan Nordström <johan@digitalvillage.se>
2022-01-27 11:53:35 +0100

    Check what the person is an object of, and if the person is an employee.

--- 04-pdo-oop/oop.php
+++ 04-pdo-oop/oop.php
@@ -23,16 +23,28 @@ $employee_query->execute();
 // get result as instances of the class Employee
 $employees = $employee_query->fetchAll(PDO::FETCH_CLASS, Employee::class);
 
+// 💥
+$people = array_merge($students, $employees);
+
 // show result
 echo "<pre>";
-echo "<h2>Employees</h2>";
-var_dump($employees);
+echo "<h2>People</h2>";
+var_dump($people);
 echo "</pre>";
 
 echo "<ul>";
-foreach ($students as $student) {
+foreach ($people as $person) {
+	$class = get_class($person); // get class (as a string) that the object is an instance of
+
+	// is user an Employee?
+	if ($person instanceof Employee) {
+		if ($person->isExternal()) {
+			$class = "external {$class}";
+		}
+	}
+
 	echo "<li>";
-	echo "{$student->getFullName()} ({$student->getEmail()}) #{$student->getId()}";
+	echo "{$person->getFullName()} ({$person->getEmail()}) is a {$class} with ID {$person->getId()}";
 	echo "</li>";
 }
 echo "</ul>";
