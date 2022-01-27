51701737619f3a16b2ab4ac65d706cd4f25126e0
Johan Nordström <johan@digitalvillage.se>
2022-01-27 10:42:26 +0100

    PDO demo (the problem with stdClass - 🥵).

--- 04-pdo-oop/assoc.php
+++ 04-pdo-oop/assoc.php
@@ -10,6 +10,7 @@ $dbh = db_init();
 $query = $dbh->prepare("SELECT * FROM students");
 $query->execute();
 
+/*
 // get result as an associative array
 $students_assoc = $query->fetchAll(PDO::FETCH_ASSOC);
 
@@ -21,6 +22,7 @@ echo "</pre>";
 
 // run query again
 $query->execute();
+*/
 
 // get result as objects
 $students_obj = $query->fetchAll(PDO::FETCH_OBJ);  // we have no idea (based on the result) where this data came from. Are they students? Are they employees? Are they zombies due to lack of coffee? YES!
@@ -28,5 +30,25 @@ $students_obj = $query->fetchAll(PDO::FETCH_OBJ);  // we have no idea (based on 
 // show result
 echo "<pre>";
 echo "<h2>Students objects</h2>";
-var_dump($students_obj);
+var_dump($students_obj[0]);
+echo "</pre>";
+
+// prepare and execute a query for employees
+$query = $dbh->prepare("SELECT * FROM employees");
+$query->execute();
+
+// get employees as objects
+$employees_obj = $query->fetchAll(PDO::FETCH_OBJ);
+
+// show result
+echo "<pre>";
+echo "<h2>Employee objects</h2>";
+var_dump($employees_obj[0]);
+echo "</pre>";
+
+$mix = [$students_obj[0], $employees_obj[0]];
+// show result
+echo "<pre>";
+echo "<h2>🥵</h2>";
+var_dump($mix);
 echo "</pre>";
