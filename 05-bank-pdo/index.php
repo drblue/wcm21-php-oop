7f614695ff6c4312f29ae666a809b7fbe32e50ab
Johan Nordström <johan@digitalvillage.se>
2022-01-27 14:09:45 +0100

    Added Bootstrap 5 with navbar and container.

--- 05-bank-pdo/index.php
+++ 05-bank-pdo/index.php
@@ -1,6 +1,9 @@
 <?php
 
 require('bootstrap/app.php');
+include('partials/header.php');
 
 $users = User::getAll();
 dump($users);
+
+include('partials/footer.php');
---
+++ 05-bank-pdo/partials/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php echo APP_NAME; ?></title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<div class="container">
+			<a class="navbar-brand" href="#"><?php echo APP_NAME; ?></a>
+
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+					<!--
+					<li class="nav-item">
+						<a class="nav-link" aria-current="page" href="#">Home</a>
+					</li>
+					-->
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<div class="container py-3">
+		<!-- here be content -->
