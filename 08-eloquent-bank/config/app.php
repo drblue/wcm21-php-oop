1207738de4c9cc713af5b66985d05e7378e3281c
Johan Nordström <johan@digitalvillage.se>
2022-02-07 11:40:23 +0100

    Added Capsule and booting of Eloquent.

--- 08-eloquent-bank/config/app.php
+++ 08-eloquent-bank/config/app.php
@@ -3,6 +3,8 @@
  * App Config
  */
 
+define('APP_NAME', 'Eloquent Bank');
+
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'bank');
 define('DB_USER', 'www');		// MAMP: `root`
---
+++ 08-eloquent-bank/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require('./bootstrap/init.php');
+
+// here be logic
+
+
+// here be output
+include(ROOT_PATH . '/views/partials/header.php');
+?>
+
+	<h1>Welcome to Bank!</h1>
+
+<?php
+include(ROOT_PATH . '/views/partials/footer.php');
