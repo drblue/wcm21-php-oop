71c961e95eee03687c6f5f1c1318fa250df28125
Johan Nordström <johan@digitalvillage.se>
2022-02-07 11:03:40 +0100

    Eloquent bank skeleton.

---
+++ 08-eloquent-bank/config/app.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * App Config
+ */
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'bank');
+define('DB_USER', 'www');		// MAMP: `root`
+define('DB_PASSWORD', 'apa');	// MAMP: `root`
