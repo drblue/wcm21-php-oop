01f589f8ff9f9e88b634d05813c3e07f6df8fb99
Johan Nordström <johan@digitalvillage.se>
2022-02-07 12:03:27 +0100

    Added Account and User model, and our first query using Eloquent.

--- 08-eloquent-bank/index.php
+++ 08-eloquent-bank/index.php
@@ -2,14 +2,26 @@
 
 require('./bootstrap/init.php');
 
-// here be logic
+use App\Models\User;
 
+// here be logic
+$users = User::all();
 
 // here be output
 include(ROOT_PATH . '/views/partials/header.php');
 ?>
 
 	<h1>Welcome to Bank!</h1>
+
+	<ul>
+		<?php foreach($users as $user): ?>
+			<li>
+				<?php echo $user->id; ?>
+				<?php echo $user->first_name; ?>
+				<?php echo $user->last_name; ?>
+			</li>
+		<?php endforeach; ?>
+	</ul>
 
 <?php
 include(ROOT_PATH . '/views/partials/footer.php');
