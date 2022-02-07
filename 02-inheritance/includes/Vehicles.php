6344eaf96d126e1e31b845d8b2aa015384ed8659
Johan Nordström <johan@digitalvillage.se>
2022-02-07 10:20:47 +0100

    Interfaces and abstract.

--- 02-inheritance/includes/Vehicles.php
+++ 02-inheritance/includes/Vehicles.php
@@ -1,19 +1,82 @@
 <?php
 
-class Vehicle {
+interface Drivable {
+	public function drive();
+}
+
+interface Engine {
+	public $fuel;
+	public $horsepower;
+
+	public function start();
+	public function stop();
+}
+
+abstract class Vehicle {
 	public $type = "Vehicle";
 	public $manufacturer;
 	public $model;
 }
 
-class Car extends Vehicle {
-	public $type = "Car";
-	public $wheels = 4;
-	public $engine = true;
+new Vehicle();
+
+class Boat extends Vehicle implements Engine {
+	public $type = "Boat";
+
+	public function start() {
+		return "Splash!";
+	}
+
+	public function stop() {
+		return "<silence>";
+	}
 }
 
-class Bicycle extends Vehicle {
+class Car extends Vehicle implements Drivable, Engine {
+	public $type = "Car";
+	public $wheels = 4;
+
+	public function drive() {
+		return "Such speed, much wow, very fast";
+	}
+
+	public function start() {
+		return "Wrooom";
+	}
+	public function stop() {
+		return "no wroom";
+	}
+}
+
+class Bicycle extends Vehicle implements Drivable {
 	public $type = "Bicycle";
 	public $wheels = 2;
 	public $engine = false;
+
+	public function drive() {
+		return "Rollin...";
+	}
+}
+
+$vehicles = [
+	new Vehicle(),
+	new Car(),
+	new Boat(),
+	new Car(),
+	new Bicycle(),
+	new Boat(),
+	new Car(),
+];
+
+foreach ($vehicles as $vehicle) {
+	if ($vehicle instanceof Drivable) {
+		echo "I can drive!<br>;";
+		$vehicle->drive();
+	}
+
+	if ($vehicle instanceof Engine) {
+		echo "I can haz engine!<br>";
+		$vehicle->start();
+		$vehicle->stop();
+	}
 }
